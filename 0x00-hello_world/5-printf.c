b92a31ce1a81352c562675b6e4682819952f051f
bidexysammy <bidexysammy@yahoo.com>
2022-09-08 08:48:38 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-gcc 5-main.c
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - this is the starting point
+* description - there is only one function
+*/
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
