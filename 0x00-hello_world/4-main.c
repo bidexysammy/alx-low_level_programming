8a2736568bcdb3e8ca9a70501ce8b8b0f958639e
bidexysammy <bidexysammy@yahoo.com>
2022-09-08 09:09:04 -0700

    using puts

--- 0x00-hello_world/4-main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-main()
-{
-   char name;
-   name = ""Programming is like building a multilingual puzzle\n";
-   puts(name);
-   return (0);
-}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc 4-main.c
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - is the only function
+ * description - this code prints strings
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
