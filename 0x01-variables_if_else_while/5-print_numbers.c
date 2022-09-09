b7f6beb80070f69b58cc5efcc33a694d3bb48d0b
bidexysammy <bidexysammy@yahoo.com>
2022-09-09 09:29:31 -0700

    5-variable

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-/*
- * main - this is found in every c program
- * desctiption - the main funciton is where compiling starts
- * return - zero if success
+
+/**
+ * main - this must be present
+ *
+ * Description: this is where the compilation starts
+ * Return: zero if success
  */
 int main(void)
 {
 	int c;
 
 	for (c = 0; c <= 9; c++)
-		putchar(c);
+	{
+		printf("%d", c);
+	}
+	printf("\n");
 	return (0);
 }
