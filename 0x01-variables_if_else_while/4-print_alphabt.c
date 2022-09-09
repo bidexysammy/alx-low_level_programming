34cbfc71838616eeb0a4c37fcfc2e0e31f26c3f2
bidexysammy <bidexysammy@yahoo.com>
2022-09-09 08:59:52 -0700

    4-variable

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-/*
+/**
  * main - this must be present in every program
- * description - the main function is where compiling begins
- * return - zero if success
+ *
+ * Description: the main function is where compiling begins
+ * Return: zero if success
  */
 int main(void)
 {
 	int c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		if (c == 'e' || c == 'q')
 			continue;
 		putchar(c);
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
