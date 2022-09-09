90d840b51c74dfe8a75f26793d2007e45c90f984
bidexysammy <bidexysammy@yahoo.com>
2022-09-09 08:47:45 -0700

    2-variable

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-/*
+/**
  * main - main must be present in every program
- * description - the main function is where compiling begins
- * return - zero if success
+ *
+ * Description: the main function is where compiling begins
+ * Return: zero if success
  */
 int main(void)
 {
 	int C;
 
 	for (C = 'a'; C <= 'z'; C++)
+	{
 		putchar(C);
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
