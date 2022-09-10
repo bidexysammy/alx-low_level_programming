55ead5d4a3a4be2b7438d2c465dc055391074d6e
bidexysammy <bidexysammy@yahoo.com>
2022-09-10 08:40:54 -0700

    9-variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this occurs in every program
+ *
+ * Description: the function main is where the compilation begins
+ * Return: zero if success
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+		if (c < 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
