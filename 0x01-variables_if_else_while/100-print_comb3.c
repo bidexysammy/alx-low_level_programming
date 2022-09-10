95f104ad8194020d4099558250d4d8cf6492b21f
bidexysammy <bidexysammy@yahoo.com>
2022-09-10 09:04:15 -0700

    10-variable

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this occurs in every program
+ *
+ * Description: the main funxction is where compiling bgins
+ * Return: zero if success
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if (c < i)
+			{
+				putchar(c + '0');
+				putchar(i + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
