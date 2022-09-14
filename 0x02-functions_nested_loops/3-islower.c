34a4808c5c8a68386cdd739df7e939a0c3448198
bidexysammy <bidexysammy@yahoo.com>
2022-09-13 20:04:32 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ *@c:int c is the argument
+ * Return: two if success
+ */
+int _islower(int c)
+{
+	int a, b;
+
+	a = 1;
+	b = 0;
+
+	if (_islower(c))
+	{
+		_putchar(a + '0');
+	}
+	else
+	{
+		_putchar(b + '0');
+	}
+	_putchar('\n');
+	return (2);
+}
+
+
