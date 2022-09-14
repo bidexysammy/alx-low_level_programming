e362a23502e544ede14bffe44b0c2994da3ddc7d
bidexysammy <bidexysammy@yahoo.com>
2022-09-13 19:07:58 -0700

    print-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet-prints a..z in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
