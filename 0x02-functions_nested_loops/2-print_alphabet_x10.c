9abaf3fc76f3b408ea1340518212c81031aa5b12
bidexysammy <bidexysammy@yahoo.com>
2022-09-13 19:29:51 -0700

    x10-alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,16 @@ void print_alphabet_x10(void)
 	int c;
 	int a;
 
-	for (a = 1; a <= 10; a++)
+	a = 1;
+
+	while (a <= 10)
 	{
-		_putchar(a);
-	}
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+		a++;
+	}
 	_putchar('\n');
 }
 
