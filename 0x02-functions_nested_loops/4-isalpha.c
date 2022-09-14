708c780cdac6da9e06afe19e730022de87924e83
bidexysammy <bidexysammy@yahoo.com>
2022-09-13 20:28:20 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - tells if its a letter
+ * @c: is the argument
+ * Return: one if  positive
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) && (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
