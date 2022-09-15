9f7e7ed4ad0cdb45d133ecc1f33cbff3fd85f108
bidexysammy <bidexysammy@yahoo.com>
2022-09-15 09:23:00 -0700

    0-main

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks if it upper
+ * @c: is the argument
+ * Return: 1 and 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
