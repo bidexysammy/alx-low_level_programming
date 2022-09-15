5725bd91238e5f7914ab26551eca0539f56ee8ae
bidexysammy <bidexysammy@yahoo.com>
2022-09-15 11:03:16 -0700

    4-main

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		if (c == 2 || c == 4)
+			continue;
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
