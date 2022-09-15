75113dbb1c6c619e95fbb96d63a5f4a991ed5656
bidexysammy <bidexysammy@yahoo.com>
2022-09-15 11:20:53 -0700

    5-main

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints more numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a;
+	int c;
+
+	a = 1;
+	while (a <= 10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c + '0');
+
+		}
+		_putchar('\n');
+		a++;
+	}
+}
