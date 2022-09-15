7eb7b7dec1aea55617085309156bf657f230c1e6
bidexysammy <bidexysammy@yahoo.com>
2022-09-15 12:11:24 -0700

    6-main

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,7 +6,23 @@
  */
 void print_line(int n)
 {
-	n = 5;
-	_putchar(45);
+	int c;
+	int i;
+
+
+	c = 95;
+	i = 0;
+
+	while (i < n)
+	{
+
+		if (n == 0 || n < 0)
+		{
+			_putchar('\n');
+		}
+
+		_putchar(c);
+		_putchar('\n');
+	}
 	return (0);
 }
