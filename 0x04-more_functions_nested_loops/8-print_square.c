6dee801045d0670fe2dddaeb75ee212a7f120265
bidexysammy <bidexysammy@yahoo.com>
2022-09-15 19:26:42 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - print n squares
+ * @size: is the argument
+ * Return: void
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
