3ea104f9e9b13dfde5e37eb99b8ff4d27d9c2b73
bidexysammy <bidexysammy@yahoo.com>
2022-09-19 07:17:33 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - this pronts the length of a string
+ * @s: is the argument
+ * Return: zero if success
+ */
+
+int _strlen(char *s)
+{
+	s = st[];
+	int i = 0;
+
+	while (st[i] = '\n')
+	{
+		_putchar((i - 1) + '0');
+		i++
+	}
+	return (0);
+}
+
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(char i)
+int _strlen(char *s)
