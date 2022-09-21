9fc7e8a30702a46dfd9769be919a99b6fb93fbe3
bidexysammy <bidexysammy@yahoo.com>
2022-09-21 11:04:33 -0700

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: is the first argument
+ * @src: is the second argument
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, m;
+	char *ptr;
+
+	dest = "Jesus";
+	src = "Christ!";
+	for (i = 0; *dest; i++)
+	{
+		while (dest = '\0')
+			continue;
+		for (m = 0; *src; m++)
+		{
+			_putchar(*ptr);
+
+		}
+	}
+	return (ptr);
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is for more of pointers, arrays and string
