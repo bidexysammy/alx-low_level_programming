6dcc7738bb108253e7dc6d461f6b3384c9c912e9
bidexysammy <bidexysammy@yahoo.com>
2022-09-28 05:36:51 -0700

    print spring

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints string
+ * @s: is the string to be printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		Return;
+	}
+	_putchar(*s);
+	s++;
+	
+}
+
+
