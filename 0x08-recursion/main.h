efa0ed7918960378581d437d424d1131fb866ed5
bidexysammy <bidexysammy@yahoo.com>
2022-09-29 07:49:33 -0700

    palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int comparator(char *s, int n1, int n2);
+
 
 int wildcmp(char *s1, char *s2);
 
