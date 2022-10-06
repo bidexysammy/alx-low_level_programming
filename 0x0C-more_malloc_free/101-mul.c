941255e379f2526c4074619e77b42bdb9d812279
bidexysammy <bidexysammy@yahoo.com>
2022-10-06 16:45:03 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,8 +6,7 @@ int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char
-		*next_prod, int next_len);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 /**
  * find_len - Finds the length of a string.
@@ -210,4 +208,3 @@ int main(int argc, char *argv[])
 	free(final_prod);
 	return (0);
 }
-
