dab5140559daed2d48278e7f35b25ccaad25d627
bidexysammy <bidexysammy@yahoo.com>
2022-10-10 18:18:29 -0700

    initialization

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,10 +9,14 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != null)
+	{
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
+	}
 
 }
 
