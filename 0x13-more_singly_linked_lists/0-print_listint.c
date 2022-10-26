23a9b5b728c71be56cbb7bfd2b425e9786b7ed75
bidexysammy <bidexysammy@yahoo.com>
2022-10-25 21:46:30 -0700

    more linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,13 @@ size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
-	if (h != NULL)
+	while (h)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
+
 	return (count);
 }
 
