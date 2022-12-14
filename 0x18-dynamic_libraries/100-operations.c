7fe371485fe2783c5ba7da9f213d20ab4185a9d4
bidexysammy <bidexysammy@yahoo.com>
2022-12-13 18:15:43 -0800

    dynamic Library

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
-int add(int a,int b)
+int main(void)
 {
-	return a + b;
+	puts("Hello World!");
+	return (0);
 }
-int sub(int a,int b)
-{
-	return a - b;
-}
-int mul(int a,int b)
-{
-	return a * b;
-}
-int div(int a,int b)
-{
-	return a / b;
-}
-int mod(int a,int b)
-{
-	return a % b;
-}
-
